88ca21a82156eafbf9b013aca0563794ebb975d1
Théo Lehéricé <18145@csmrouen.net>
2020-01-09 17:03:36 +0100

    cv

--- includes/cv.inc.php
+++ includes/cv.inc.php
@@ -5,9 +5,23 @@
     <title>Théo Lehéricé - Référent Digital</title>
   </head>
   <body>
-    <div class="fdprofile">
 
+    <div class="fdprofile">
+      <img src="./assets/img/noelprofile.jpg" alt="Frédéric Noël el patron">
     </div>
-    <img src="./assets/img/noelprofile.jpg" alt="Frédéric Noël el patron">
+
+    <div class="nomprenompostecv">
+      <h1>Frédéric Noël - Joueur de triangle</h1>
+    </div>
+
+    <div class="experienceprocv">
+      <h1>Expérience Professionnelles</h1>
+      <ul>
+        <li>Octobre 2012 - Décembre 2014 | Boulanger [BOULANGERIE NOËL]</li>
+        <li>Janvier 2015 - Novembre 2016 | Batteur groupe de rock avec une baguette de pain [BAGUETTE BOYSBAND]</li>
+        <li>Février 2017 - Janvier 2020 | Accompagnateur d'assisté mentaux [NFACTORY SCHOOL]</li>
+      </ul>
+    </div>
+
   </body>
 </html>
