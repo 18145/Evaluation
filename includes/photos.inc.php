f4cce181c50abf1c5105eceb0e19c42f7149862b
Théo Lehéricé <18145@csmrouen.net>
2020-01-10 13:00:30 +0100

    push

--- includes/photos.inc.php
+++ includes/photos.inc.php
@@ -1,7 +1,11 @@
-<div class="beach">
+<div id="beach">
   <img src="./assets/img/beach.jpg" alt="plage">
 </div>
 
-<div class="foret">
+<div id="foret">
   <img src="./assets/img/foret.jpg" alt="forêt">
+</div>
+
+<div id="textphotos">
+  <h1>Mes photos de vacance</h1>
 </div>
