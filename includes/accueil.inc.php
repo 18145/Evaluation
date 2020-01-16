719308c45f1395a72d39d52a06325d326b1909b4
Théo Lehéricé <18145@csmrouen.net>
2020-01-16 16:57:39 +0100

    Bonjour !

--- includes/accueil.inc.php
+++ includes/accueil.inc.php
@@ -1,4 +1,7 @@
 <body>
+  <script type="text/javascript">
+    alert("Bonjour");
+  </script>
    <div class="no">
   <p> Lorem ididunt ut labore et doloDuis aute irureore eu fugiat nuidunt.cvnfvnjfjnvvnfv njvfnjvnfvjnfv jnvnvfnfvjnfvnjvn</p>
   <p> Lorem ididunt ut labore et doloDuis aute irureore eu fugiat nuidunt.cvnfvnjfjnvvnfv njvfnjvnfvjnfv jnvnvfnfvjnfvnjvn</p>
